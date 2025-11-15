73d36725be0e0dfd6a0ccefdbef9075bc4b80ee3
JavvajiPraveenKumar <praveenjavvaji6552@gmail.com>
2025-11-15 16:39:27 +0530

    this is made by six branch

--- git/index.js
+++ git/index.js
@@ -3,8 +3,10 @@ console.log(name)
 
 let text="this was written by tenth branch"
 console.log(text)
-let text='this was modified by nine branch'
+let tex='this was modified by nine branch'
 console.log(text)
 
 
 let hey="Hey how are ??"
+let lastname="kumar"
+console.log(lastname)
